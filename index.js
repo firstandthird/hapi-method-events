44f1cbbe015a177334fe8d45fdf32499f4dd8620
orthagonal <orthagonal1@gmail.com>
2017-12-28 18:44:25 -0600

    only log in verbose mode, add event tags

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const register = function(server, options) {
     server.events.on(eventData.event, async(data) => {
       try {
         const result = await str2fn.execute(eventData.method, server.methods, data);
-        server.log(['hapi-method-events', eventData.method], result);
+        if (options.verbose) {
+          const eventName = typeof eventData.event === 'object' ? eventData.event.name : eventData.event;
+          server.log(['hapi-method-events', eventName, eventData.method], result);
+        }
       } catch (err) {
-        return server.log(err);
+        return server.log(['hapi-method-events', eventName, eventData.method], err);
       }
     });
   });
